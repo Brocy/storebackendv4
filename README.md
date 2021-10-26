5f44a43a81a2809bd766afa1c1e84c46ae2b5c04
brocycenteio <centeio.3face@gmail.com>
2021-10-26 15:37:07 -0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Strapi V4 beta application
+Strapi V4 beta in Docker Compose.
+
+## How to run Strapi on Docker
+1. Start Docker
+2. Run ``docker-compose up``
+3. Go to http://localhost:4000/admin
---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2d3916349ce87833184336d60a8897c1'),
+    },
+  },
+});
